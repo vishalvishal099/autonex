3f6cfdb4b7d48e0b386da9f1cc3959e60a408cdd
Vishal Singh <vsingh@dhcp-10-120-176-35.wifi.oracle.com>
2019-11-29 11:52:30 +0530

    added runner and parallel tags for pom

--- src/test/java/com/oracle/babylon/jobRunner/LoginTest.java
+++ src/test/java/com/oracle/babylon/jobRunner/LoginTest.java
@@ -1,4 +1,4 @@
-package com.oracle.babylon;
+package com.oracle.babylon.jobRunner;
 
 
 import cucumber.api.CucumberOptions;
@@ -6,14 +6,14 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/com/oracle/babylon/",
-        tags = {"~@ignore"},
+@CucumberOptions(features = "src/test/resources/com/oracle/babylon/features/login.feature",
+        tags = {"@one"},
         glue= {"com.oracle.babylon"},
         plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty", "json:target/cucumber-reports/cucumber.json" },
         monochrome = true
 )
 
-public class RunCucumberTest {
+public class LoginTest {
 
 }
 
---
+++ src/test/java/com/oracle/babylon/jobRunner/RunCucumberTest.java
@@ -0,0 +1,19 @@
+package com.oracle.babylon.jobRunner;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/com/oracle/babylon/",
+        tags = {"~@ignore"},
+        glue= {"com.oracle.babylon"},
+        plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty", "json:target/cucumber-reports/cucumber.json" },
+        monochrome = true
+)
+
+public class RunCucumberTest {
+
+}
+
