fdb32e86867cdef3028cd19a3347a509c01edfae
Vishal Singh <vsingh@dhcp-10-191-248-209.vpn.oracle.com>
2019-11-29 15:26:09 +0530

    Added tag to runner file

--- src/test/java/com/oracle/babylon/jobRunner/LoginTest.java
+++ src/test/java/com/oracle/babylon/jobRunner/LoginTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/com/oracle/babylon/features/login.feature",
-        tags = {"@one"},
+        tags = {"~@ignore"},
         glue= {"com.oracle.babylon"},
         plugin = { "pretty", "html:target/cucumber-reports/cucumber-pretty", "json:target/cucumber-reports/cucumber.json" },
         monochrome = true
